01dc3f9dc07a66f9fbb2e238a3dd0c7c31628037
typoworx-de <info@typoworx.de>
2016-01-19 18:43:40 +0100

    Create imagemap.js

---
+++ imagemap.js
@@ -0,0 +1,12 @@
+/*
+ * trace coordinates
+ */
+$('img[usemap]').click(function(e){
+    var offset = $(this).offset(),
+        size = 8,
+        mapX = Math.floor(e.pageX - offset.left),
+        mapY = Math.floor(e.pageY - offset.top)
+    ;
+
+    console.log('RECT', [mapX, mapY, mapX+size, mapY+size].join(','));
+});
