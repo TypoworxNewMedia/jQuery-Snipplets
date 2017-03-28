5ed19e6efec18204c9ac4adc0ac800a9c2190dcc
typoworx-de <info@typoworx.de>
2017-03-28 14:30:41 +0200

    Create safe-words-nonbreakable.js

---
+++ helper/safe-words-nonbreakable.js
@@ -0,0 +1,32 @@
+var tryModule = require('tryModule').add;
+
+var safeWords = [
+    'foo', 'bar', 'foo-bar'
+].join(',');
+
+tryModule(function() {
+    if (safeWords.length) {
+        var safeWordsRegex = new RegExp(safeWords.regexEscape().replace(/,/g, '|'), 'g');
+
+        $('body').find('a').each(function () {
+            $(this).contents()
+                .filter(function () {
+                    return $(this).text().toString().match(safeWordsRegex);
+                })
+                .each(function () {
+                    var $parentNode = this.parentNode,
+                        fulltext = $(this).text(),
+                        rewrite = ''
+                    ;
+
+                    fulltext.replace(safeWordsRegex, function (s1) {
+                        rewrite = fulltext.replace(s1, '<span class="nobr">' + s1 + '</span>');
+                        if (rewrite != fulltext) {
+                            $($parentNode).html(rewrite);
+                        }
+                    });
+                });
+            ;
+        });
+    }
+}, 'safeWords');
