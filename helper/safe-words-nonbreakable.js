911de286d59339f62e17b46f6474ae596c1a9203
typoworx-de <info@typoworx.de>
2017-03-28 14:31:21 +0200

    Update safe-words-nonbreakable.js

--- helper/safe-words-nonbreakable.js
+++ helper/safe-words-nonbreakable.js
@@ -1,32 +1,35 @@
 var tryModule = require('tryModule').add;
+var jQuery = require('jQuery').add;
 
 var safeWords = [
     'foo', 'bar', 'foo-bar'
 ].join(',');
 
-tryModule(function() {
-    if (safeWords.length) {
-        var safeWordsRegex = new RegExp(safeWords.regexEscape().replace(/,/g, '|'), 'g');
+(function($) {
+    tryModule(function() {
+        if (safeWords.length) {
+            var safeWordsRegex = new RegExp(safeWords.regexEscape().replace(/,/g, '|'), 'g');
 
-        $('body').find('a').each(function () {
-            $(this).contents()
-                .filter(function () {
-                    return $(this).text().toString().match(safeWordsRegex);
-                })
-                .each(function () {
-                    var $parentNode = this.parentNode,
-                        fulltext = $(this).text(),
-                        rewrite = ''
-                    ;
+            $('body').find('a').each(function () {
+                $(this).contents()
+                    .filter(function () {
+                        return $(this).text().toString().match(safeWordsRegex);
+                    })
+                    .each(function () {
+                        var $parentNode = this.parentNode,
+                            fulltext = $(this).text(),
+                            rewrite = ''
+                        ;
 
-                    fulltext.replace(safeWordsRegex, function (s1) {
-                        rewrite = fulltext.replace(s1, '<span class="nobr">' + s1 + '</span>');
-                        if (rewrite != fulltext) {
-                            $($parentNode).html(rewrite);
-                        }
+                        fulltext.replace(safeWordsRegex, function (s1) {
+                            rewrite = fulltext.replace(s1, '<span class="nobr">' + s1 + '</span>');
+                            if (rewrite != fulltext) {
+                                $($parentNode).html(rewrite);
+                            }
+                        });
                     });
-                });
-            ;
-        });
-    }
-}, 'safeWords');
+                ;
+            });
+        }
+    }, 'safeWords');
+})(jQuery, window, document);
