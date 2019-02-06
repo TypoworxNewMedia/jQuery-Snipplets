3d131c6878c4d97d229958f0fc09cda38683e6da
typoworx-de <info@typoworx.de>
2019-02-06 16:10:05 +0100

    Create app.bootstrap.js

---
+++ requireJS/app.bootstrap.js
@@ -0,0 +1,29 @@
+'use strict';
+
+(function(baseUrl, appRoot) {
+    window.module = {exports: {}};
+    
+    window._environment = window._environment instanceof Object ? window._environment : {};
+    window._environment.appContext = window._environment.hasOwnProperty('appContext') ? window._environment.appContext : 'Production';
+
+    requirejs.config({
+        appDir: appRoot,
+        baseUrl: baseUrl,
+        urlArgs: _environment.appContext && _environment.appContext === 'Development' ? 'bust=' + (new Date()).getTime() : '',
+        paths: {
+            app: 'app',
+            environment: 'modules/environment',
+            jQuery: '3rd-party/jquery.min',
+            jQueryUi: '3rd-party/jquery-ui-1.10.2.min',
+            jQueryMask: '3rd-party/jquery.mask.min',
+            openIban: '3rd-party/openiban.browser'
+        },
+        shim: {
+            'jQuery': {exports: 'jQuery'},
+            'environment': {'deps': ['jQuery']},
+            'jQueryMask': {'deps': ['jQuery']}
+        },
+        deps: ["app"],
+        out: "app.compiled.js"
+    });
+})('/typo3conf/ext/my-siteload-ext/Resources/Public/Js/', '/');
