2fc24395f511c25cd5eca7ca847127fab647eb39
typoworx-de <info@typoworx.com>
2020-08-27 16:35:14 +0200

    Update simple-dom-components-loader.js

--- js/simple-dom-components-loader.js
+++ js/simple-dom-components-loader.js
@@ -1,17 +1,19 @@
-$(document).on('ready ajax.ready', function(e) {
-  // e.target -> domDoc is either window.document or ajax scope (if given in AJAX request)
-  (function(domDoc) {
-    $('div[data-module]', domDoc).each(function() {
-      ///console.log('Pre-Init data-module', $(this).data('module'), $(this).prop('id'));
+(function() {  
+  $(document).on('ready ajax.ready', function(e) {
+    // e.target -> domDoc is either window.document or ajax scope (if given in AJAX request)
+    (function(domDoc) {
+      $('div[data-module]', domDoc).each(function() {
+        ///console.log('Pre-Init data-module', $(this).data('module'), $(this).prop('id'));
 
-      if($(this).data('module') === 'google-map')
-      {
-        if ($(this).children().length === 0 && !$(this).data('module-init'))
+        if($(this).data('module') === 'google-map')
         {
-          $(this).data('module-init', true);
-          google.maps.event.addDomListener(window, 'load', StdwMaps.init($(this).prop('id')));
+          if ($(this).children().length === 0 && !$(this).data('module-init'))
+          {
+            $(this).data('module-init', true);
+            google.maps.event.addDomListener(window, 'load', StdwMaps.init($(this).prop('id')));
+          }
         }
-      }
-    });
-  })(e.target);
-});
+      });
+    })(e.target);
+  });
+})(jQuery);
