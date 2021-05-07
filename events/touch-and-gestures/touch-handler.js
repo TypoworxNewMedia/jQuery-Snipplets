1f4d97e6de2bfd5506c89351deb7cc8a3e0f511a
typoworx-de <info@typoworx.de>
2021-05-07 13:52:21 +0200

    Update touch-handler.js

--- events/touch-and-gestures/touch-handler.js
+++ events/touch-and-gestures/touch-handler.js
@@ -1,16 +1,34 @@
+/**
+ * Example:
+ * $(this)
+ *  .touchHandler()
+ *  .on('left.move.touch right.move.touch', function(e) {
+ *      if(e.type === 'left')
+ *      {
+ *          $(this).flexslider('prev');
+ *      }
+ *      else
+ *      {
+ *          $(this).flexslider('next');
+ *      }
+ *   })
+ * ;
+ **/
+
 (function ($) {
-    $.fn.touchHandler = function() {
+$.fn.touchHandler = function() {
         $(this).on('touchstart touchend mousedown mouseup touchmove mousemove', function(e) {
             e.preventDefault();
 
             var offset = [ e.clientX, e.clientY ],
-                isDragging = $(this).data('dragging-move');
+                isDragging = $(this).data('dragging-move')
             ;
 
             if(e.type === 'touchstart' || e.type === 'mousedown')
             {
                 //console.log('drag-start');
                 isDragging = true;
+
                 $(this).trigger('start.touch');
                 $(this).data('dragging-enabled', true);
                 $(this).data('dragging-offset', offset);
@@ -20,7 +38,7 @@
                 //console.log('drag-end');
                 isDragging = false;
                 $(this).data('dragging', false);
-                $(this).trigger('end.touch');
+                $(this).trigger('end.touch', offset);
 
                 var dragOffset = $(this).data('dragging-offset');
                 var dragRelative = { top: 0, left: 0, bottom: 0, right: 0};
@@ -28,28 +46,28 @@
                 if(offset[0] > dragOffset[0])
                 {
                     //console.log('drag move right');
-                    $(e.target).trigger('right.drag');
+                    $(e.target).trigger('left.move.touch');
                 }
                 else if(offset[0] < dragOffset[0])
                 {
                     //console.log('drag move left');
-                    $(e.target).trigger('left.drag');
+                    $(e.target).trigger('right.move.touch');
                 }
 
                 if(offset[1] > dragOffset[1])
                 {
                     //console.log('drag move down');
-                    $(e.target).trigger('down.drag');
+                    $(e.target).trigger('down.move.touch');
                 }
                 else if(offset[1] < dragOffset[1])
                 {
                     //console.log('drag move up');
-                    $(e.target).trigger('up.drag');
+                    $(e.target).trigger('up.move.touch');
                 }
 
                 dragRelative.top = offset[1] - dragOffset[1];
                 dragRelative.left = offset[0] - dragOffset[0];
-                $(e.target).trigger('offset.drag', dragRelative);
+                $(e.target).trigger('endmove.touch', dragRelative);
 
                 $(this).data('dragging-offset', [0,0]);
             }
@@ -57,12 +75,12 @@
             if(isDragging === true)
             {
                 //console.log('drag moving');
-                $(this).trigger('move.touch');
+                $(this).trigger('move.touch', offset);
                 $(this).data('dragging-offset', offset);
             }
-
-            $(this).data('dragging-move', isDragging);
         });
+
+        return $(this);
     };
 })
 (jQuery);
